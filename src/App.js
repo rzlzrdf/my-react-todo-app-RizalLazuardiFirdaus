4d8b0ba714605fb29b47c8658c875862212e9dd3
rzlzrdf <rizallazu@student.ub.ac.id>
2022-05-18 22:23:17 +0700

    step 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const items = [
+    {
+     id: 1,
+     text: 'Membuang sampah',
+     completed: false
+   },
+   {
+     id: 2,
+     text: 'Membuat rotu',
+     completed: false
+   },
+   {
+     id: 3,
+     text: 'Belajar React',
+     completed: false
+   }
+ ];
+ const title = 'Things to do';
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className="row">
+      <div className="todo-list">
+        <h1>{title}</h1>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="todo-item-check-1" />
+              <label className="form-check-label" htmlFor="todo-item-check-1">Membuang sampah</label>
+            </div>
+          </li>
+          <li className="list-group-item">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="todo-item-check-2" />
+              <label className="form-check-label" htmlFor="todo-item-check-2">Membeli roti</label>
+            </div>
+          </li>
+          <li className="list-group-item">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="todo-item-check-3" />
+              <label className="form-check-label" htmlFor="todo-item-check-3">Membuat kopi</label>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
   );
 }
 
