c6861cbf11f6ea6aeba9c799348c38c006ec5f8c
rzlzrdf <rizallazu@student.ub.ac.id>
2022-05-19 12:36:13 +0700

    step 1 done

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -3,7 +3,7 @@ const TodoItem = (props) => {
   return (
     <>
         <div className="form-check">
-            <input className="form-check-input" type="checkbox" value="true" id={`todo-item-check-${props.item.id}`}  />
+            <input className="form-check-input" type="checkbox" value={`todo-item-check-${props.item.completed}`} id={`todo-item-check-${props.item.id}`}  />
             <label className="form-check-label" htmlFor={`todo-item-check-${props.item.id}`}>
                 {props.item.text}
             </label>
