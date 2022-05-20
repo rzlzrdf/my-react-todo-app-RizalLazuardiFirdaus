09ec8b29c9a965005c92e7f87074999042159e99
rzlzrdf <rizallazu@student.ub.ac.id>
2022-05-20 15:51:03 +0700

    step 2 done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = (props) => {
   return (
     <div className="todo-footer">
-        <span className="count-todos"><h6>{props.items.length}{props.items.length > 1 ? ' item left' : ' items left'}</h6></span> {/* buatlah sebuah ternary operation di mana apabila props count > 1 maka akan menuliskan text "items left" dan jika props count <= 1 maka akan menuliskan "item left" */}
+        <span className="count-todos"><h6>{props.items.length}{props.items.length > 1 ? ' items left' : ' item left'}</h6></span> {/* buatlah sebuah ternary operation di mana apabila props count > 1 maka akan menuliskan text "items left" dan jika props count <= 1 maka akan menuliskan "item left" */}
     </div>
   )
 }
