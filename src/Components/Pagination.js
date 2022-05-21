6e77a442e343ba664b3ab6f25b0629abfd85dce5
rzlzrdf <rizallazu@student.ub.ac.id>
2022-05-21 10:36:59 +0700

    step 3 done

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,58 +1,55 @@
 import React, { useState, useEffect } from 'react'
+import TodoItem from './TodoItem'
 
 const PaginateList = (props) => {
 
-    // buatlah sebuah state yang bernama displayedItems dengan initial value sebuah array kosong
-    const [displayedItems, setDisplayedItems] =  useState([])
-
-    // buatlah sebuah state yang bernama currentPage dengan initial value props.initialPage
+    const [displayedItems, setDisplayedItems] = useState([])
     const [currentPage, setCurrentPage] = useState(props.initialPage)
 
     useEffect(() => {
         const startingIndex = ( currentPage - 1 ) * props.itemPerPage
         const endIndex = startingIndex + props.itemPerPage
-        const newDisplayedItems = props.items.filter((item, index) => index >= startingIndex && index < endIndex) /* filter props.items untuk mendapatkan item dari index 'startingIndex' sampai dengan 1 index sebelum 'endIndex' */
+        const newDisplayedItems = props.items.filter( (item, index) => (
+            index >= startingIndex && index < endIndex
+        ))
         setDisplayedItems(newDisplayedItems)
     }, [currentPage, props.itemPerPage, props.items])
 
     const totalPage = Math.ceil(props.items.length / props.itemPerPage)
 
     const goToPrev = () => {
-        // Apabila currentPage lebih besar dari 1 maka state currentPage ditambah 1
-        if (currentPage > 1) {
-            setCurrentPage(currentPage-1)
+        if ( currentPage > 1 ) {
+            setCurrentPage( (prevState) => {
+                return prevState - 1
+            })
         }
     }
 
     const goToNext = () => {
-        // Apabila currentPage lebih kecil dari totalPage maka state currentPage dikurangi 1
-        if (currentPage < totalPage) {
-            setCurrentPage(currentPage + 1)
+        if ( currentPage < totalPage ) {
+            setCurrentPage( (prevState) => {
+                return prevState + 1
+            })
         }
     }
 
     return (
         <>
             <ul className="list-group list-group-flush">
-				{displayedItems.map((item) => (
-					<li className="list-group-item" key={item.id}>
-						{item.text}
-					</li>
-				))}
-			</ul>
-			{totalPage > 0 && (
-				<div className="page-controller">
-					<button className="page-btn" onClick={goToPrev}>
-						Prev
-					</button>
-					page {currentPage} of {totalPage}
-					<button className="page-btn" onClick={goToNext}>
-						Next
-					</button>
-				</div>
-			)}
+                {displayedItems.map( displayItem => (
+                    <li key={displayItem.id} className="list-group-item">
+                        <TodoItem item={displayItem}/>
+                    </li>
+                ))}
+            </ul>
+            { ( totalPage > 0 ) && 
+                <div className="page-controller">
+                    <button className="page-btn" onClick={goToPrev}>Prev</button>
+                    page {currentPage} of {totalPage}
+                    <button className="page-btn" onClick={goToNext}>Next</button>
+                </div>
+            }
         </>
-
     )
 }
 
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,24 +1,18 @@
 import React from 'react'
 import Header from './Header'
-import TodoItem from './TodoItem'
 import Footer from './Footer'
-import PaginateList from './Pagination'
+import Pagination from "./Pagination";
 
 const Todolist = (props) => {
 
     return (
         <div className="todo-list">
-         <Header title={props.title.toUpperCase()}  addNewItem={props.addNewItem}/>
-        <ul className="list-group list-group-flush">
-            {props.items.map(item => (
-            <li className="list-group-item">
-                <TodoItem item={item}/>
-            </li>
-            ))}
-        </ul>
-        <PaginateList items={props.items} initialPage={1} itemPerPage={2} />
-        <Footer items={props.items} />
-        </div>
+            <Header title={props.title.toUpperCase()}  addNewItem={props.addNewItem}/>
+                <ul className="list-group list-group-flush">
+                    <Pagination items={props.items} initialPage={1} itemPerPage={2} />
+                </ul>
+            <Footer count={props.items.length}/>
+        </div>  
     )
 }
 
